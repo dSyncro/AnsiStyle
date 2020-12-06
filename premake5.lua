59b7df3968ec7d598e37adf1c0f828a9b5761f98
unknown <mancini.domenico456@gmail.com>
2020-12-06 18:53:53 +0100

    Fixed premake dependencies

--- premake5.lua
+++ premake5.lua
@@ -25,47 +25,4 @@ workspace "AnsiStyle"
 
 	outputdir = "%{cfg.architecture}/%{cfg.system}/%{cfg.buildcfg}"
 
-project "AnsiStyle"
-
-	location "project"
-
-	kind "StaticLib"
-	language "C++"
-	cppdialect "C++17"
-	staticruntime "on"
-
-	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("%{wks.location}/bin/intermediates/" .. outputdir .. "/%{prj.name}")
-
-	files
-	{
-		"**.h",
-		"**.cpp",
-	}
-
-	includedirs
-	{
-		"./",
-	}
-
-	filter "system:windows"
-		systemversion "latest"
-
-	filter "system:linux"
-		systemversion "latest"
-
-	filter "configurations:Debug"
-		defines "DEBUG"
-		runtime "Debug"
-		symbols "on"
-
-	filter "configurations:Release"
-		defines "RELEASE"
-		runtime "Release"
-		optimize "on"
-
-	filter "configurations:Dist"
-		defines "DIST"
-		runtime "Release"
-		optimize "on"
-
+include "project"
---
+++ project/premake5.lua
@@ -0,0 +1,42 @@
+project "AnsiStyle"
+
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
+
+	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("%{wks.location}/bin/intermediates/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"**.h",
+		"**.cpp",
+	}
+
+	includedirs
+	{
+		"./",
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "system:linux"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		defines "DEBUG"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines "RELEASE"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		defines "DIST"
+		runtime "Release"
+		optimize "on"
+
